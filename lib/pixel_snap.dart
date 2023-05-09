d73313b5426b2072ce56e358697d75dfccdcad8b
Matej Knopp <matej.knopp@gmail.com>
2023-05-09 15:38:30 +0200

    fix: missing imports

--- lib/pixel_snap.dart
+++ lib/pixel_snap.dart
@@ -1,5 +1,5 @@
 export 'src/pixel_snap.dart';
-export 'src/forked/raw_image.dart';
+export 'src/pixel_snap_ext.dart';
 export 'src/widgets/debug_bar/debug_bar.dart';
 export 'src/widgets/pixel_ratio_override.dart';
 export 'src/widgets/pixel_snap_size.dart';
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,2 +1,3 @@
 export 'src/generated/widgets.dart';
+export 'src/forked/raw_image.dart';
 export 'pixel_snap.dart';
